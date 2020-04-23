57bbfec162cae05693c86d918f9e8c591a01b33a
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-23 09:40:45 +0400

    change email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
 
-  validates :name, presence: true, length: { minimum: 2 }
+  validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true, format: { with: '@'}, uniqueness: true
+  validates :email, presence: true, format: { with: /\A.*@.*\z/ }, uniqueness: true
 end
