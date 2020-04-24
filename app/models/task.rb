50762e4d87e12fcc8a6b5da3f2cdbc6c8f07cad2
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-24 08:56:59 +0400

    implement tasks state

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,30 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine :initial => :new_task do
+    event :put_in_development do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+
+    event :put_in_qa do
+      transition :in_development => :in_qa
+    end
+
+    event :put_in_archived do
+      transition [:new_task, :released] => :archived
+    end
+
+    event :put_in_code_review do
+      transition :in_qa => :in_code_review
+    end
+
+    event :put_in_ready_for_release do
+      transition :in_code_review => :ready_for_release
+    end
+
+    event :put_in_release do
+      transition :ready_for_release => :released
+    end
+  end
 end
