1a41f1c3d592a4773aea73070a6f1ea99e7fc137
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-24 08:58:10 +0400

    add uniq_index by email

---
+++ db/migrate/20200424044808_add_index_by_email_to_users.rb
@@ -0,0 +1,5 @@
+class AddIndexByEmailToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_061520) do
+ActiveRecord::Schema.define(version: 2020_04_24_044808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_061520) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
