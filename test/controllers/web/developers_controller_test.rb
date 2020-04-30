aa6a98570e67b50c0c041b6003cbde2f7ffac87d
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-04-30 08:20:20 +0400

    add checks for developer

--- test/controllers/web/developers_controller_test.rb
+++ test/controllers/web/developers_controller_test.rb
@@ -4,10 +4,19 @@ class Web::DevelopersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+
   end
 
   test "should post create" do
-    post :create, params: { developer: attributes_for(:developer) }
+    attrs = attributes_for(:developer)
+    post :create, params: { developer: attrs }
     assert_response :redirect
+
+    email = attrs[:email]
+    developer = Developer.find_by(email: email)
+    assert developer.present?
+
+    assert signed_in?
+    assert current_user == developer
   end
 end
