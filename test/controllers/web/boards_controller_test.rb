acad02e23f2018f16746b90e42e548b594e7a4d5
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-05-06 12:58:39 +0400

    add partials for edit and new.html.slim

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -3,8 +3,15 @@
 require 'test_helper'
 
 class Web::BoardsControllerTest < ActionDispatch::IntegrationTest
+  before_action :authenticate_user!
+
   test 'should get show' do
     get board_url
     assert_response :success
+  end
+
+  setup do
+    user = create(:user)
+    sign_in user
   end
 end
