7e99ce937f89e952cc1126fa78485e50d39e8341
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-05-08 17:13:20 +0400

    update traits to use associations

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -6,11 +6,11 @@ FactoryBot.define do
     description
 
     trait :with_author do
-      author { create(:manager) }
+      author factory: :manager
     end
 
     trait :with_assignee do
-      assignee { create(:developer) }
+      assignee factory: :developer
     end
   end
 end
