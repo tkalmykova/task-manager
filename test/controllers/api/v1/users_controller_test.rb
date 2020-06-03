d5832ac26e1b8cf0a4787eba7fe5f9602aae5cb1
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-06-03 13:59:34 +0400

    fix rubocop remarks

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
-require 'test helper'
-
+require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionController::TestCase
-  test "should get show" do
-    user = create :user,
-    get :show, params: {id: user.id, format: :json}
-    assert_response: success
+  test 'should get show' do
+    user = create(:user)
+    get :show, params: { id: user.id, format: :json }
+    assert_response :success
   end
 
-  test "should get index" do
-    get: index, params: {format: :json}
-    assert_response: success
+  test 'should get index' do
+    get :index, params: { format: :json }
+    assert_response :success
   end
 end
