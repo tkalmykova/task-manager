334b7ed9fd995ded8621ba34d638ed4ae934a3fd
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-06-09 08:29:52 +0400

    Revert "remive author select"
    This reverts commit d5beadc3fb3779825036eb7c276a826b51648ed1.

--- app/javascript/forms/TaskForm.js
+++ app/javascript/forms/TaskForm.js
@@ -19,6 +19,7 @@ export default {
     return {
       ...pick(pertmittedKeys, task),
       assigneeId: propOr(null, 'id', task.assignee),
+      authorId: propOr(null, 'id', task.author),
     };
   },
 };
