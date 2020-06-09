0caf06f7b009f7618bd2e4902933fd4bf947f440
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-06-09 09:14:53 +0400

    remove author

--- app/javascript/forms/TaskForm.js
+++ app/javascript/forms/TaskForm.js
@@ -19,7 +19,6 @@ export default {
     return {
       ...pick(pertmittedKeys, task),
       assigneeId: propOr(null, 'id', task.assignee),
-      authorId: propOr(null, 'id', task.author),
     };
   },
 };
