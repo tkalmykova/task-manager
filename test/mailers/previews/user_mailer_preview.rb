1ef8f0273293628fc46f498bd083fba61781c449
Tatiana Kalmykova <ul.tkalmykova@gmail.com>
2020-06-17 11:56:36 +0400

    add letter_opener

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,25 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def task_created
+    user = User.first
+    task = Task.first
+    params = { user: user, task: task }
 
+    UserMailer.with(params).task_created
+  end
+
+  def task_destroyed
+    user = User.first
+    task = Task.first
+    params = { user: user, task: task }
+
+    UserMailer.with(params).task_destroyed
+  end
+
+  def task_updated
+    user = User.first
+    task = Task.first
+    params = { user: user, task: task }
+
+    UserMailer.with(params).task_updated
+  end
 end
